86c53b59b70fcf7b068ea097fb7e074b6a630d0a
himanshugharat <himanshugharat44@gmail.com>
2020-07-01 15:41:58 +0530

    switch case

--- employe.sh
+++ employe.sh
@@ -3,13 +3,14 @@ user=$((RANDOM%2))
 total=0
 wages=20
 hours=8
-if [ $user -eq 1 ]
-then
+case $user in
+1)
 	echo "if you have done over time then enter the hrs"
 	read ot
 	echo "present"
 	total=$((wages*hours))
-	total=$((total+$((wages*ot))))
-else
-	echo "absent"
-fi
+	total=$((total+$((wages*ot))));;
+0)
+	echo "absent";;
+
+esac
