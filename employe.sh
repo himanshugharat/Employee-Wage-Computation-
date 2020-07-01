813de91e7829c9f9d5759202a37dc514543f7ef1
himanshugharat <himanshugharat44@gmail.com>
2020-07-01 16:05:59 +0530

    done for 20 days

--- employe.sh
+++ employe.sh
@@ -1,16 +1,24 @@
 #!/bin/bash -x
-user=$((RANDOM%2))
 total=0
+#wages=20
+#hours=8
+for((i=0;i<10;i++))
+do
+#total=0
+#total=0
 wages=20
 hours=8
+user=$((RANDOM%2))
 case $user in
 1)
 	echo "if you have done over time then enter the hrs"
 	read ot
 	echo "present"
-	total=$((wages*hours))
+	total=$((total+$((wages*hours))))
 	total=$((total+$((wages*ot))));;
 0)
 	echo "absent";;
 
 esac
+done
+echo $total
