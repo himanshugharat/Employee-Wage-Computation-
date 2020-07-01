d7f2d016bb922aa4ae9f1499567673f239105aaf
himanshugharat <himanshugharat44@gmail.com>
2020-07-01 15:36:54 +0530

    added the overtime

--- employe.sh
+++ employe.sh
@@ -1,10 +1,15 @@
 #!/bin/bash -x
 user=$((RANDOM%2))
-wages=0
+total=0
+wages=20
+hours=8
 if [ $user -eq 1 ]
 then
+	echo "if you have done over time then enter the hrs"
+	read ot
 	echo "present"
-	wages=$((20*8))
+	total=$((wages*hours))
+	total=$((total+$((wages*ot))))
 else
 	echo "absent"
 fi
